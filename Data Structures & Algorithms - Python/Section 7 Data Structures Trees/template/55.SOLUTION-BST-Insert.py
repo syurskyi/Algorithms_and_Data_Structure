8aa5d14429442857975fc1f9086404856e70139c
serge <sergejyurskyj@yahoo.com>
2022-11-13 14:31:09 -0800

    Commit Data Structures & Algorithms - Python, Section 7 Data Structures Trees

--- Data Structures & Algorithms - Python/Section 7 Data Structures Trees/template/55.SOLUTION-BST-Insert.py
+++ Data Structures & Algorithms - Python/Section 7 Data Structures Trees/template/55.SOLUTION-BST-Insert.py
@@ -1,61 +1,61 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-        
-
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value):
-        new_node = Node(value)
-        if self.root is None:
-            self.root = new_node
-            return True
-        temp = self.root
-        while (True):
-            if new_node.value == temp.value:
-                return False
-            if new_node.value < temp.value:
-                if temp.left is None:
-                    temp.left = new_node
-                    return True
-                temp = temp.left
-            else: 
-                if temp.right is None:
-                    temp.right = new_node
-                    return True
-                temp = temp.right
-
-
-
-
-my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
-
-"""
-    THE LINES ABOVE CREATE THIS TREE:
-                 2
-                / \
-               1   3
-"""
-
-
-print('Root:', my_tree.root.value)            
-print('Root->Left:', my_tree.root.left.value)        
-print('Root->Right:', my_tree.root.right.value)        
-
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Root: 2
-    Root->Left: 1
-    Root->Right: 3
-
-"""
+# c_ Node
+#     ___ -  value
+#         ? _ ?
+#         l.. _ N..
+#         r.. _ N..
+#
+#
+# c_ BinarySearchTree
+#     ___ -r.. _ N..
+#         r.. _ N..
+#
+#     ___ insert  v..
+#         n.. _ ? ?
+#         __ ? __ N..
+#             r.. __ ?
+#             r.. T..
+#         t.. _ ?
+#         ____ T..
+#             __ ?.v.. __ ?.v..
+#                 r_ F..
+#             __ ?.v.. < ?.v..
+#                 __ ?.l.. __ N..
+#                     ?.l.. _ ?
+#                     r.. T..
+#                 t.. _ ?.l..
+#             ____
+#                 __ ?.r.. __ N..
+#                     ?.r.. _ ?
+#                     r.. T..
+#                 t.. _ ?.r..
+#
+#
+#
+#
+# my_tree = ?
+# ?.i.. 2
+# ?.i.. 1
+# ?.i.. 3
+#
+# """
+#     THE LINES ABOVE CREATE THIS TREE:
+#                  2
+#                 / \
+#                1   3
+# """
+#
+#
+# print('Root:', ?.r__.v..
+# print('Root->Left:', ?.r__.l...v..
+# print('Root->Right:', ?.r__.r...v..
+#
+#
+#
+# """
+#     EXPECTED OUTPUT:
+#     ----------------
+#     Root: 2
+#     Root->Left: 1
+#     Root->Right: 3
+#
+# """
