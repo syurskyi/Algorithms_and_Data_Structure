ea79cfe6b2ed83ac350295ff2c192a1d24b47839
serge <sergejyurskyj@yahoo.com>
2022-11-12 22:09:26 -0800

    Commit Data Structures & Algorithms - Python, Section Section 6 Data Structures Stacks & Queues exercise

--- Data Structures & Algorithms - Python/Section 6 Data Structures Stacks & Queues/exercise/44.SOLUTION-Stack-Push.py
+++ Data Structures & Algorithms - Python/Section 6 Data Structures Stacks & Queues/exercise/44.SOLUTION-Stack-Push.py
@@ -1,53 +1,53 @@
-# c_ Node
-#     ___ -  value
-#         ? _ ?
-#         n.. _ N..
-#
-#
-# c_ Stack
-#     ___ -  value
-#         n... _ ? ?
-#         t.. _ ?
-#         h. _ 1
-#
-#     ___ print_stack
-#         t.. _ t..
-#         _____  ? __ n.. N..
-#             print ?.v..
-#             ? _ ?.n..
-#
-#     ___ push value
-#         n... _ ? ?
-#         __ ? __ 0
-#             t.. _ ?
-#         ____
-#             ?.n.. _ t..
-#             ? _ ?
-#         ? +_ 1
-#
-#
-#
-#
-# my_stack = ? 2
-#
-# print('Stack before push(1):')
-# ?.p..
-#
-# ?.p.. 1
-#
-# print('\nStack after push(1):')
-# ?.p..
-#
-#
-#
-# """
-#     EXPECTED OUTPUT:
-#     ----------------
-#     Stack before push(1):
-#     2
-#
-#     Stack after push(1):
-#     1
-#     2
-#
-# """
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
+
+
+
+my_stack = Stack(2)
+
+print('Stack before push(1):')
+my_stack.print_stack()
+z
+my_stack.push(1)
+
+print('\nStack after push(1):')
+my_stack.print_stack()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Stack before push(1):
+    2
+
+    Stack after push(1):
+    1
+    2
+
+"""
