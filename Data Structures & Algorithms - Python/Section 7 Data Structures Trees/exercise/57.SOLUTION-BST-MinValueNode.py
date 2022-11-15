9d1480858810c62bb3b8e1c4cf6c675e6c625094
serge <sergejyurskyj@yahoo.com>
2022-11-14 17:42:56 -0800

    Commit Data Structures & Algorithms - Python, Section 7 Data Structures Trees

--- Data Structures & Algorithms - Python/Section 7 Data Structures Trees/exercise/57.SOLUTION-BST-MinValueNode.py
+++ Data Structures & Algorithms - Python/Section 7 Data Structures Trees/exercise/57.SOLUTION-BST-MinValueNode.py
@@ -1,72 +1,67 @@
-# c_ Node
-#     ___ -  v..
-#         ? _ ?
-#         l.. _ N..
-#         r.. _ N..
-#
-#
-# c_ BinarySearchTree
-#     ___ -
-#         r.. _ N..
-#
-#     ___ insert  v..
-#         n.. _ ? ?
-#         __ ? __ N..
-#             r.. __ ?
-#             r.. T..
-#         t.. _ ?
-#         ____ T..
-#             __ ?.v.. __ ?.v..
-#                 r_ F..
-#             __ ?.v.. < ?.v..
-#                 __ ?.l.. __ N..
-#                     ?.l.. _ ?
-#                     r.. T..
-#                 t.. _ ?.l..
-#             ____
-#                 __ ?.r.. __ N..
-#                     ?.r.. _ ?
-#                     r.. T..
-#                 t.. _ ?.r..
-#
-#     ___ contains  v..
-#         t.. _ ?
-#         _____ ? __ n.. N..
-#             __ v.. < ?.v..
-#                 t.. _ ?.l..
-#             ____ v.. > ?.v..
-#                 t.. _ ?.r..
-#             ____
-#                 r.. T..
-#         r_ F..
-#
-#     ___ min_value_node  current_node
-#         _____ ?.l.. __ n.. N..
-#             ? _ ?.l..
-#         r_ ?
-#
-#
-#
-#
-# my_tree = ?
-# ?.i.. 47
-# ?.i.. 21
-# ?.i.. 76
-# ?.i.. 18
-# ?.i.. 27
-# ?.i.. 52
-# ?.i.. 82
-#
-#
-# print('Minimum Value in Tree:')
-# print( ?.m..(m__.r..).v.. )
-#
-#
-#
-# """
-#     EXPECTED OUTPUT:
-#     ----------------
-#     Minimum Value in Tree:
-#     18
-#
-# """
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+    def min_value_node(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print('Minimum Value in Tree:')
+print(my_tree.min_value_node(my_tree.root).value)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Minimum Value in Tree:
+    18
+
+"""
