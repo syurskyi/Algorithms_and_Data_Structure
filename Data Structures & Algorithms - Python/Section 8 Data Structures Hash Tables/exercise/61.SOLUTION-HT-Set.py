af9b8607734c56a9eda3142ff73ed1693710e0b9
serge <sergejyurskyj@yahoo.com>
2022-11-14 22:59:04 -0800

    Commit Data Structures & Algorithms - Python Section 8 Data Structures Hash Tables exrecise

--- Data Structures & Algorithms - Python/Section 8 Data Structures Hash Tables/exercise/61.SOLUTION-HT-Set.py
+++ Data Structures & Algorithms - Python/Section 8 Data Structures Hash Tables/exercise/61.SOLUTION-HT-Set.py
@@ -1,45 +1,45 @@
-# c_ HashTable
-#     ___ -  s.. _ 7
-#         d.. _ N.. * ?
-#
-#     ___ print_table
-#         ___ i, val __ e__ ?
-#             print ? ": " ?
-#
-#     ___ __hash  key
-#         m.. _ 0
-#         ___ l.. __ ?
-#             m.. _ ? + o.. ? * 23) % l.. ?
-#         r_ ?
-#
-#     ___ set_item  k.. v..
-#         i.. _ ? ?
-#         __ ? ? __ N..
-#             ? ?   # list
-#         ? ?.a.. ? ?
-#
-#
-#
-#
-# my_hash_table = ?
-#
-# ?.s.. 'bolts', 1400
-# ?.s.. 'washers', 50
-# ?.s.. 'lumber', 70
-#
-# ?.p..
-#
-#
-#
-# """
-#     EXPECTED OUTPUT:
-#     ----------------
-#     0 :  None
-#     1 :  None
-#     2 :  None
-#     3 :  None
-#     4 :  [['bolts', 1400], ['washers', 50]]
-#     5 :  None
-#     6 :  [['lumber', 70]]
-#
-# """
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []  # list
+        self.data_map[index].append([key, value])
+
+
+
+
+my_hash_table = ?
+
+?.s.. 'bolts', 1400
+?.s.. 'washers', 50
+?.s.. 'lumber', 70
+
+?.p..
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    0 :  None
+    1 :  None
+    2 :  None
+    3 :  None
+    4 :  [['bolts', 1400], ['washers', 50]]
+    5 :  None
+    6 :  [['lumber', 70]]
+
+"""
