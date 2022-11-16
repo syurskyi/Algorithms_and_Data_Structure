7f9e9171b1908e823b3061c493cdb9e44cb6c2b0
serge <sergejyurskyj@yahoo.com>
2022-11-15 18:19:10 -0800

    Commit Data Structures & Algorithms - Python

--- Data Structures & Algorithms - Python/Section 5 Data Structures Doubly Linked Lists/exercise/41.SOLUTION-DLL-Remove.py
+++ Data Structures & Algorithms - Python/Section 5 Data Structures Doubly Linked Lists/exercise/41.SOLUTION-DLL-Remove.py
@@ -1,35 +1,35 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.prev = None
-
-
-class DoublyLinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
-
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
-
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-        self.length += 1
-        return True
-
+# c_ Node
+#     ___  -  value
+#         ? _ ?
+#         n.. _ N..
+#         p.. _ N..
+#
+#
+# c_ DoublyLinkedList
+#     ___  -  value
+#         n.. _ ? ?
+#         h.. _ ?
+#         t.. _ ?
+#         l.. _ 1
+#
+#     ___ print_list
+#         t.. _ h..
+#         w__ ? __ n.. N..
+#             print ?.v..
+#             ? _ ?.n..
+#
+#     ___ append  value
+#         n.. _ ? ?
+#         __ ? __ N..
+#             h.. _ ?
+#             t.. _ ?
+#         ____
+#             t__.n.. _ ?
+#             ?.p.. _ t..
+#             t.. _ ?
+#         ? =_ 1
+#         r_ T..
+#
 #     ___ pop
 #         __ ? __ 0
 #             r_ N..
@@ -44,18 +44,18 @@ class DoublyLinkedList:
 #         ? -_ 1
 #         r_ ?
 #
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-        self.length += 1
-        return True
-
+#     ___ prepend  value
+#         n.. _ ? ?
+#         __ ? __ 0
+#             h.. _ ?
+#             t.. _ ?
+#         ____
+#             ?.n.. _ h..
+#             ?.p.. _ ?
+#             h.. _ ?
+#         ? =_ 1
+#         r_ T..
+#
 #     ___ pop_first
 #         __ ? __ 0
 #             r_ N..
@@ -70,19 +70,19 @@ class DoublyLinkedList:
 #         ? -_ 1
 #         r_ ?
 #
-    def get(self, index):
-        # __ ? < 0 __ ? >_ ?
-        #     r_ N..
-        temp = self.head
-        # __ ? < ?/2
-        #     ___ _ __ r_ ?
-        #         ? _ ?.n..
-        # ____
-        #     t.. _ t..
-        #     ___ _ __ r_ ? -1 ? -1
-        #         ? _ ?.p..
-        return temp
-
+#     ___ get  index
+#         __ ? < 0 __ ? >_ ?
+#             r_ N..
+#         t.. _ h..
+#         __ ? < ?/2
+#             ___ _ __ r_ ?
+#                 ? _ ?.n..
+#         ____
+#             t.. _ t..
+#             ___ _ __ r_ ? -1 ? -1
+#                 ? _ ?.p..
+#         r_ ?
+#
 #     ___ set_value  ? value
 #         t.. _ g.. ?
 #         __ ?
@@ -90,70 +90,72 @@ class DoublyLinkedList:
 #             r_ T..
 #         r_ F..
 #
-    def insert(self, index, value):
-        if index < 0 or index > self.length:
-            return False
-        if self.length == 0:
-            return self.prepend(value)
-        if index == self.length:
-            return self.append(value)
-
-        new_node = Node(value)
-        before = self.get(index - 1)
-        after = before.next
-
-        new_node.prev = before
-        new_node.next = after
-        before.next = new_node
-        after.prev = new_node
-
-        self.length += 1
-        return True
-
-    def remove(self, index):
-        if index < 0 or index >= self.length:
-            return None
-        __ ? __ 0
-            r_ p..
-        __ ? __ ? - 1
-            r_ p..
-
-        t.. _ get ?
-
-        ?.n__.p.. _ ?.p..
-        ?.p...n.. _ ?.n..
-        t__.n.. _ N..
-        ?.p.. _ N..
-
-        ? -_ 1
-        r_ ?
-
-
-
-my_doubly_linked_list = DoublyLinkedList(1)
-my_doubly_linked_list.append(2)
-my_doubly_linked_list.append(3)
-my_doubly_linked_list.append(4)
-my_doubly_linked_list.append(5)
-
-print('DLL before remove():')
-my_doubly_linked_list.print_list()
-
-print('\nRemoved node:')
-print(my_doubly_linked_list.remove(2).value)
-print('DLL after remove() in middle:')
-my_doubly_linked_list.print_list()
-
-print('\nRemoved node:')
-print(my_doubly_linked_list.remove(0).value)
-print('DLL after remove() of first node:')
-my_doubly_linked_list.print_list()
-
-print('\nRemoved node:')
-print(my_doubly_linked_list.remove(2).value)
-print('DLL after remove() of last node:')
-my_doubly_linked_list.print_list()
-
+#     ___ insert  ? value
+#         __ ? < 0 __ ? > ?
+#             r_ F..
+#         __ ? __ 0
+#             r_ p.. ?
+#         __ ? __ ?
+#             r_ a.. ?
+#
+#         n.. _ ? ?
+#         b.. _ g.. ? - 1
+#         a.. _ ?.n..
+#
+#         ?.p.. _ b..
+#         ?.n.. _ a..
+#         ?.n.. _ ?
+#         ?.p.. _ ?
+#
+#         ? =_ 1
+#         r_ T..
+#
+#     ___ remove  index
+#         __ ? < 0 __ ? >_ ?
+#             r_ N..
+#         __ ? __ 0
+#             r_ p..
+#         __ ? __ ? - 1
+#             r_ p..
+#
+#         t.. _ get ?
+#
+#         ?.n__.p.. _ ?.p..
+#         ?.p...n.. _ ?.n..
+#         t__.n.. _ N..
+#         ?.p.. _ N..
+#
+#         ? -_ 1
+#         r_ ?
+#
+#
+#
+#
+# my_doubly_linked_list = ? 1
+# ?.a.. 2
+# ?.a.. 3
+# ?.a.. 4
+# ?.a.. 5
+#
+# print('DLL before remove():')
+# ?.p..
+#
+# print('\nRemoved node:')
+# print(?.r.. 2 .v..
+# print('DLL after remove() in middle:')
+# ?.p..
+#
+# print('\nRemoved node:')
+# print(?.r.. 0 .v..
+# print('DLL after remove() of first node:')
+# ?.p..
+#
+# print('\nRemoved node:')
+# print(?.r.. 2 .v..
+# print('DLL after remove() of last node:')
+# ?.p..
+#
+#
 #
 # """
 #     EXPECTED OUTPUT:
--- Data Structures & Algorithms - Python/Section 9 Data Structures Graphs/exercise/73.SOLUTION-GR-Remove_Vertex.py
+++ Data Structures & Algorithms - Python/Section 9 Data Structures Graphs/exercise/73.SOLUTION-GR-Remove_Vertex.py
@@ -1,82 +1,82 @@
-class Graph:
-    def __init__(self):
-        self.adj_list = {}  # dict
-
-    def print_graph(self):
-        for vertex in self.adj_list:
-            print(vertex, ':', self.adj_list[vertex])
-
-    def add_vertex(self, vertex):
-        if vertex not in self.adj_list.keys():
-            self.adj_list[vertex] = [] # lsit
-            return True
-        return False
-
-    def add_edge(self, v1, v2):
-        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            self.adj_list[v1].append(v2)
-            self.adj_list[v2].append(v1)
-            return True
-        return False
-
-    def remove_edge(self,v1, v2):
-        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            try:
-                self.adj_list[v1].remove(v2)
-                self.adj_list[v2].remove(v1)
-            except ValueError:
-                pass
-            return True
-        return False
-
-    def remove_vertex(self, vertex):
-        if vertex in self.adj_list.keys():
-            for other_vertex in self.adj_list[vertex]:
-                self.adj_list[other_vertex].remove(vertex)
-            del self.adj_list[vertex]
-            return True
-        return False
-
-
-
-
-my_graph = ?
-?.a..'A'
-?.a..'B'
-?.a..'C'
-?.a..'D'
-
-?.a..'A','B'
-?.a..'A','C'
-?.a..'A','D'
-?.a..'B','D'
-?.a..'C','D'
-
-
-print('Graph before remove_vertex():')
-?.p..
-
-
-?.r.. 'D'
-
-
-print('\nGraph after remove_vertex():')
-?.p..
-
-
-
-"""
-EXPECTED OUTPUT:
-----------------
-    Graph before remove_vertex():
-    A : ['B', 'C', 'D']
-    B : ['A', 'D']
-    C : ['A', 'D']
-    D : ['A', 'B', 'C']
-
-    Graph after remove_vertex():
-    A : ['B', 'C']
-    B : ['A']
-    C : ['A']
-
-"""
+# c_ Graph
+#     ___  -
+#         adj_list _  # dict
+#
+#     ___ print_graph
+#         ___ v.. __ ?
+#             print ? ':' ? ?
+#
+#     ___ add_vertex v..
+#         __ ? n.. __ ?.k..
+#             ? ?_  # lsit
+#             r_ T..
+#         r_ F..
+#
+#     ___ add_edge  v1, v2
+#         __ ? __ ?.k.. ___ ? __ ?.k..
+#             ? ?.a.. ?
+#             ? ?.a.. ?
+#             r_ T..
+#         r_ F..
+#
+#     ___ remove_edge  v1, v2
+#         __ ? __ ?.k.. ___ ? __ ?.k..
+#             ___
+#                 ? ?.r.. ?
+#                 ? ?.r.. ?
+#             e.. V..
+#                 p..
+#             r_ T..
+#         r_ F..
+# # other_vertex
+#     ___ remove_vertex  v..
+#         __ ? __ ?.k..
+#             ___ ? __ ? ?
+#                 ? ?.r.. ?
+#             d__ ? ?
+#             r_ T..
+#         r_ F..
+#
+#
+#
+#
+# my_graph = ?
+# ?.a..'A'
+# ?.a..'B'
+# ?.a..'C'
+# ?.a..'D'
+#
+# ?.a..'A','B'
+# ?.a..'A','C'
+# ?.a..'A','D'
+# ?.a..'B','D'
+# ?.a..'C','D'
+#
+#
+# print('Graph before remove_vertex():')
+# ?.p..
+#
+#
+# ?.r.. 'D'
+#
+#
+# print('\nGraph after remove_vertex():')
+# ?.p..
+#
+#
+#
+# """
+# EXPECTED OUTPUT:
+# ----------------
+#     Graph before remove_vertex():
+#     A : ['B', 'C', 'D']
+#     B : ['A', 'D']
+#     C : ['A', 'D']
+#     D : ['A', 'B', 'C']
+#
+#     Graph after remove_vertex():
+#     A : ['B', 'C']
+#     B : ['A']
+#     C : ['A']
+#
+# """
