7e5014e45fa6d432e11ba23ac100ceddf0c065a7
serge <sergejyurskyj@yahoo.com>
2022-11-19 13:40:38 -0800

    Commit templates preparation

--- _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/add two numbers/index.py
+++ _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/add two numbers/index.py
@@ -1,32 +1,32 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-c_ Solution
-    ___ addTwoNumbers l1: ListNode, l2: ListNode) __ ListNode:
-        ans _ ListNode(N..)
-        pointer _ ans
-
-        carry _ 0
-        sum _ 0
-
-        _____(l1!_None __ l2!_None
-            sum _ carry
-            __(l1!_None
-                sum+_l1.val
-                l1 _ l1.n..
-            __(l2!_None
-                sum+_l2.val
-                l2 _ l2.val
-            
-            carry _ i..(sum/10)
-            pointer.n..  _ ListNode(sum%10)
-
-            pointer _ pointer.n..
-        
-        __(carry>0
-            pointer.n.. _ ListNode(carry)
-        
-        r_ ans.n..
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+#
+# c_ Solution
+#     ___ addTwoNumbers l1 L.. l2 L.. __ L..
+#         ans _ ? N..
+#         pointer _ ?
+#
+#         carry _ 0
+#         sum _ 0
+#
+#         _____ ?!_N.. __ ?!_N..
+#             s.. _ ?
+#             __ ?!_N..
+#                 ?+_?.v..
+#                 ? _ ?.n..
+#             __ ?!_N..
+#                 ?+_?.v..
+#                 ? _ ?.v..
+#
+#             c.. _ i.. s../10)
+#             ?.n..  _ L.. s__%10
+#
+#             ? _ ?.n..
+#
+#         __ ?>0
+#             ?.n.. _ ? ?
+#
+#         r_ ?.n..
