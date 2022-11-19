4b151fc9c0570929f9149ff2bd9a4dad03e44cbd
serge <sergejyurskyj@yahoo.com>
2022-11-19 13:49:09 -0800

    Commit templates preparation

--- _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/merge 2 sorted lists/index.py
+++ _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/merge 2 sorted lists/index.py
@@ -1,30 +1,30 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-c_ Solution
-    ___ mergeTwoLists l1: ListNode, l2: ListNode) __ ListNode:
-		cur _ ListNode(0)
-		ans _ cur
-		
-		_____(l1 ___ l2
-			__(?.v..>?.v..
-				?.n.. _ l2
-				l2 _ ?.n..
-			____
-				?.n.. _ l1
-				l1 _ ?.n..
-			cur _ ?.n..
-			
-		_____(l1
-			?.n.. _ l1
-			l1 _ ?.n..
-			cur _ ?.n..
-		
-		_____(l2
-			?.n.. _ l2
-			l2 _ ?.n..
-			cur _ ?.n..
-		r_ ans.n..
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+#
+# c_ Solution
+#     ___ mergeTwoLists l1 L.. l2 L.. __ L.
+# 		cur _ ? 0
+# 		ans _ ?
+#
+# 		_____ ? ___ ?
+# 			__(?.v..>?.v..
+# 				?.n.. _ l2
+# 				l2 _ ?.n..
+# 			____
+# 				?.n.. _ l1
+# 				l1 _ ?.n..
+# 			cur _ ?.n..
+#
+# 		_____ ?
+# 			?.n.. _ l1
+# 			l1 _ ?.n..
+# 			cur _ ?.n..
+#
+# 		_____ ?
+# 			?.n.. _ l2
+# 			l2 _ ?.n..
+# 			cur _ ?.n..
+# 		r_ ?.n..
--- _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/odd even linkedlist/index.py
+++ _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/odd even linkedlist/index.py
@@ -1,24 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-c_ Solution
-    ___ oddEvenList head: ListNode) __ ListNode:
-        __(n.. head
-            r_ head
-
-        odd _ head
-        even _ odd.n..
-        evenList _ even
-
-        _____(even ___ even.n..
-            odd.n.. _ even.n..
-            odd _ odd.n..
-
-            even.n.. _ odd.n..
-            even _ even.n..
-
-        odd.n.. _ evenList
-        r_ head
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+#
+# c_ Solution
+#     ___ oddEvenList head L.. __ L..
+#         __ n.. ?
+#             r_ ?
+#
+#         odd _ ?
+#         even _ ?.n..
+#         evenList _ ?
+#
+#         _____ ? ___ ?.n..
+#             o__.n.. _ ?.n..
+#             o.. _ ?.n..
+#
+#             ?.n.. _ o__.n..
+#             ? _ ?.n..
+#
+#         o__.n.. _ ?
+#         r_ ?
