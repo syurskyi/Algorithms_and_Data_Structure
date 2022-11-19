8219020f951e60e86b368df02cf9bf13520f4f57
serge <sergejyurskyj@yahoo.com>
2022-11-19 13:41:47 -0800

    Commit templates preparation

--- _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/linked list cycle/index.py
+++ _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/linked list cycle/index.py
@@ -1,11 +1,11 @@
-c_ Solution
-    ___ hasCycle head: ListNode) __ b..:
-        hare _ head
-        turtle _ head
-
-        _____ turtle ___ hare ___ hare.n..:
-            hare _ hare.n...next
-            turtle _ turtle.n..
-            __(turtle __ hare
-                r_ T..
-        r_ F..
+# c_ Solution
+#     ___ hasCycle head L.. __ b..
+#         hare _ ?
+#         turtle _ ?
+#
+#         _____ ? ___ ? ___ ?.n..
+#             h.. _ ?.n...n..
+#             t... _ ?.n..
+#             __ ? __ ?
+#                 r_ T..
+#         r_ F..
