b3678770cc3cee9c57e9dfa8c7d603826f329c6b
serge <sergejyurskyj@yahoo.com>
2022-11-19 17:47:51 -0800

    Commit templates preparation

--- _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/reverse linked list/index.py
+++ _temp/LeetCode In Python/template/Section 12 INTERVIEW QUESTIONS Linked Lists/reverse linked list/index.py
@@ -1,16 +1,16 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
-c_ Solution
-    ___ reverseList head: ListNode) __ ListNode:
-        node _ N..
-        _____(head __ n.. N..
-            next _ head.n..
-            head.n.. _ node
-            node _ head
-            head _ next
-        r_ node
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+#
+#
+# c_ Solution
+#     ___ reverseList head L.. __ L..
+#         node _ N..
+#         _____ ? __ n.. N..
+#             next _ ?.n..
+#             ?.n.. _ n..
+#             n.. _ ?
+#             ? _ ?
+#         r_ ?
