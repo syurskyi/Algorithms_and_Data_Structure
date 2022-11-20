30883121713f29fd9b2625565470920ece5cc337
serge <sergejyurskyj@yahoo.com>
2022-11-19 18:43:09 -0800

    Commit templates preparation

--- _temp/Python for Data Structures Algorithms/template/12 Array Sequences/Array Sequences Interview Questions/SOLUTIONS/String Compression -SOLUTION.py
+++ _temp/Python for Data Structures Algorithms/template/12 Array Sequences/Array Sequences Interview Questions/SOLUTIONS/String Compression -SOLUTION.py
@@ -1,74 +1,74 @@
-# String Compression
-# Problem
+# # String Compression
+# # Problem
+# #
+# # Given a string in the form 'AAAABBBBCCCCCDDEEEE' compress it to become 'A4B4C5D2E4'. For this problem, you can
+# # falsely "compress" strings of single or double letters. For instance, it is okay for 'AAB' to return 'A2B1' even
+# # though this technically takes more space.
+# # The function should also be case sensitive, so that a string 'AAAaaa' returns 'A3a3'.
+# # Solution
+# # Since Python strings are immutable, we'll need to work off of a list of characters, and at the end convert that list
+# # back into a string with a join statement.
+# # The solution below should yield us with a Time and Space complexity of O(n). Let's take a look with careful attention
+# # to the explanatory comments:
 #
-# Given a string in the form 'AAAABBBBCCCCCDDEEEE' compress it to become 'A4B4C5D2E4'. For this problem, you can
-# falsely "compress" strings of single or double letters. For instance, it is okay for 'AAB' to return 'A2B1' even
-# though this technically takes more space.
-# The function should also be case sensitive, so that a string 'AAAaaa' returns 'A3a3'.
-# Solution
-# Since Python strings are immutable, we'll need to work off of a list of characters, and at the end convert that list
-# back into a string with a join statement.
-# The solution below should yield us with a Time and Space complexity of O(n). Let's take a look with careful attention
-# to the explanatory comments:
-
-___ compress(s
-    """
-    This solution compresses without checking. Known as the RunLength Compression algorithm.
-    """
-
-    # Begin Run as empty string
-    r _ ""
-    l _ l..(s)
-
-    # Check for length 0
-    __ l __ 0:
-        r_ ""
-
-    # Check for length 1
-    __ l __ 1:
-        r_ s + "1"
-
-    # Intialize Values
-    last _ s[0]
-    cnt _ 1
-    i _ 1
-
-    _____ i < l:
-
-        # Check to see if it is the same letter
-        __ s[i] __ s[i - 1]:
-            # Add a count if same as previous
-            cnt +_ 1
-        ____
-            # Otherwise store the previous data
-            r _ r + s[i - 1] + s..(cnt)
-            cnt _ 1
-
-        # Add to index count to terminate while loop
-        i +_ 1
-
-    # Put everything back into run
-    r _ r + s[i - 1] + s..(cnt)
-
-    r_ r
-
-compress('AAAAABBBBCCCC')
-# 'A5B4C4'
-
-# Test Your Solution
-"""
-RUN THIS CELL TO TEST YOUR SOLUTION
-"""
-____ n___.t.. ______ assert_equal
-
-c_ TestCompress o..
-
-    ___ test sol
-        ? ? ''), '')
-        ? ? 'AABBCC'), 'A2B2C2')
-        ? ? 'AAABCCDDDDD'), 'A3B1C2D5')
-        print('ALL TEST CASES PASSED')
-
-# Run Tests
-t _ TestCompress()
-t.test(compress)
+# ___ compress s
+#     """
+#     This solution compresses without checking. Known as the RunLength Compression algorithm.
+#     """
+#
+#     # Begin Run as empty string
+#     r _ ""
+#     l _ l.. ?
+#
+#     # Check for length 0
+#     __ l __ 0
+#         r_ ""
+#
+#     # Check for length 1
+#     __ l __ 1
+#         r_ ? + "1"
+#
+#     # Intialize Values
+#     last _ ? 0
+#     cnt _ 1
+#     i _ 1
+#
+#     _____ ? < ?
+#
+#         # Check to see if it is the same letter
+#         __ ? ? __ ? ? - 1
+#             # Add a count if same as previous
+#             ? +_ 1
+#         ____
+#             # Otherwise store the previous data
+#             r _ ? + ? ? - 1] + s.. ?
+#             ? _ 1
+#
+#         # Add to index count to terminate while loop
+#         ? +_ 1
+#
+#     # Put everything back into run
+#     r _ ? + ? ? - 1 + s.. ?
+#
+#     r_ ?
+#
+# compress('AAAAABBBBCCCC')
+# # 'A5B4C4'
+#
+# # Test Your Solution
+# """
+# RUN THIS CELL TO TEST YOUR SOLUTION
+# """
+# ____ n___.t.. ______ a...
+#
+# c_ TestCompress o..
+#
+#     ___ test sol
+#         ? ? ''), '')
+#         ? ? 'AABBCC'), 'A2B2C2')
+#         ? ? 'AAABCCDDDDD'), 'A3B1C2D5')
+#         print('ALL TEST CASES PASSED')
+#
+# # Run Tests
+# t _ TestCompress()
+# t.test(compress)
--- _temp/Python for Data Structures Algorithms/template/12 Array Sequences/Array Sequences Interview Questions/SOLUTIONS/Unique Characters in String - SOLUTION.py
+++ _temp/Python for Data Structures Algorithms/template/12 Array Sequences/Array Sequences Interview Questions/SOLUTIONS/Unique Characters in String - SOLUTION.py
@@ -1,44 +1,44 @@
-# Unique Characters in String
-# Problem
+# # Unique Characters in String
+# # Problem
+# #
+# # Given a string,determine if it is compreised of all unique characters. For example, the string 'abcde' has all
+# # unique characters and should return True. The string 'aabcde' contains duplicate characters and should return false.
+# # Solution
+# #
+# # We'll show two possible solutions, one using a built-in data structure and a built in function, and another using
+# # a built-in data structure but using a look-up method to check if the characters are unique
 #
-# Given a string,determine if it is compreised of all unique characters. For example, the string 'abcde' has all
-# unique characters and should return True. The string 'aabcde' contains duplicate characters and should return false.
-# Solution
+# ___ uni_char s
+#     r_ l.. s.. ? __ l.. ?
 #
-# We'll show two possible solutions, one using a built-in data structure and a built in function, and another using
-# a built-in data structure but using a look-up method to check if the characters are unique
-
-___ uni_char(s
-    r_ l..(s..(s)) __ l..(s)
-
-___ uni_char2(s
-    chars _ s..
-    ___ let __ s:
-        # Check if in set
-        __ let __ chars:
-            r_ F..
-        ____
-            #Add it to the set
-            chars.add(let)
-    r_ T..
-
-# Test Your Solution
-"""
-RUN THIS CELL TO TEST YOUR CODE>
-"""
-____ n___.t.. ______ assert_equal
-
-
-c_ TestUnique o..
-
-    ___ test sol
-        ? ? ''), T..)
-        ? ? 'goo'), F..)
-        ? ? 'abcdefg'), T..)
-        print('ALL TEST CASES PASSED')
-
-
-# Run Tests
-t _ TestUnique()
-t.test(uni_char)
-
+# ___ uni_char2 s
+#     chars _ s..
+#     ___ let __ ?
+#         # Check if in set
+#         __ ? __ ?
+#             r_ F..
+#         ____
+#             #Add it to the set
+#             ?.a.. ?
+#     r_ T..
+#
+# # Test Your Solution
+# """
+# RUN THIS CELL TO TEST YOUR CODE>
+# """
+# ____ n___.t.. ______ a..
+#
+#
+# c_ TestUnique o..
+#
+#     ___ test sol
+#         ? ? ''), T..)
+#         ? ? 'goo'), F..)
+#         ? ? 'abcdefg'), T..)
+#         print('ALL TEST CASES PASSED')
+#
+#
+# # Run Tests
+# t _ TestUnique()
+# t.test(uni_char)
+#
