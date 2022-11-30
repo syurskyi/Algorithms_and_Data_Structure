f7f9dab868eb5c4aafd49d4b6e91b100ff8e6355
serge <sergejyurskyj@yahoo.com>
2022-11-29 17:40:50 -0800

    Committed stack exercises

--- Python Data Structures and Algorithms/exercise/5. Stacks/3.2 Stack - Array-List Based.py
+++ Python Data Structures and Algorithms/exercise/5. Stacks/3.2 Stack - Array-List Based.py
@@ -1,41 +1,42 @@
-# ____ e.. _______ E..
-#
-#
-# c_ ArrayStack
-#     ___ -
-#         _data _  # list
-#
-#     ___ -l
-#         r_ l.. ?
-#
-#     ___ is_empty
-#         r_ l.. ? __ 0
-#
-#     ___ push e
-#         ?.a.. ?
-#
-#     ___ pop
-#         __ ?
-#             r_ ? 'Stack is Empty'
-#         r_ ?.p..
-#
-#     ___ top
-#         __ ?
-#             r_ ? 'Stack is Empty'
-#         r_ ? -1
-#
-# s _ ArrayStack()
-# s.push(10)
-# s.push(20)
-# print('Stack: ', s._data)
-# print('Length: ', l..(s))
-# print('Is-Empty: ', s.is_empty())
-# print ( 'Popped: ', s. pop())
-# print('Stack: ', s._data)
-# print ( 'Popped: ', s. pop())
-# print( 'Is-Empty: ', s.is_empty())
-# print('Stack: ', s._data)
-# s.push(30)
-# s.push(40)
-# print('Top Element: ', s.top())
-# print('Stack: ', s._data)
+from exceptions import Empty
+
+
+class ArrayStack:
+    def __init__(self):
+        self._data = [] # list
+
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, e):
+        self._data.append(e)
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is Empty')
+        return self._data.pop
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is Empty')
+        return self._data[-1]
+
+s = ArrayStack()
+s.push(10)
+s.push(20)
+print('Stack: ', s._data)
+print('Length: ', len(s))
+print('Is-Empty: ', s.is_empty())
+print ( 'Popped: ', s. pop())
+print('Stack: ', s._data)
+print ( 'Popped: ', s. pop())
+print( 'Is-Empty: ', s.is_empty())
+print('Stack: ', s._data)
+s.push(30)
+s.push(40)
+print('Top Element: ', s.top())
+print('Stack: ', s._data)
+
