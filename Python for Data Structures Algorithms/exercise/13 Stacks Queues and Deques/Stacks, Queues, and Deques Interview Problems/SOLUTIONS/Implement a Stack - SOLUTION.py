e82035e710cfa62ffc1aa90c6ff5a1a6027a922b
serge <sergejyurskyj@yahoo.com>
2022-11-29 18:44:53 -0800

    Committed stack exercises

--- Python for Data Structures Algorithms/exercise/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/SOLUTIONS/Implement a Stack - SOLUTION.py
+++ Python for Data Structures Algorithms/exercise/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/SOLUTIONS/Implement a Stack - SOLUTION.py
@@ -1,26 +1,26 @@
-# # %%
-# '''
-# # Implement a Stack -SOLUTION
-# **Please refer to the lecture on implementation for a full explanation. Here is the code from that lecture:**
-# '''
-#
-# # %%
-# c_ Stack o..
-#
-#     ___ -
-#         items _    # list
-#
-#     ___ isEmpty
-#         r_ ? __    # list
-#
-#     ___ push item
-#         ?.a.. ?
-#
-#     ___ pop
-#         r_ ?.p..
-#
-#     ___ peek
-#         r_ ? l.. ?-1
-#
-#     ___ size
-#         r_ l.. ?
+# %%
+'''
+# Implement a Stack -SOLUTION
+**Please refer to the lecture on implementation for a full explanation. Here is the code from that lecture:**
+'''
+
+# %%
+class Stack(object):
+
+    def __init__(self):
+        self.items = []    # list
+
+    def isEmpty(self):
+        return self.items == []   # list
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items-1)]
+
+    def size(self):
+        return len(self.items)
