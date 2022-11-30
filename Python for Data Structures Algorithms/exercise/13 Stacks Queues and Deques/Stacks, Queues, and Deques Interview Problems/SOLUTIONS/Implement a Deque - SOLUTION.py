7e073f3c5546191beb33c9ca6b24734a4ef50ab4
serge <sergejyurskyj@yahoo.com>
2022-11-29 19:04:57 -0800

    Committed stack exercises

--- Python for Data Structures Algorithms/exercise/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/SOLUTIONS/Implement a Deque - SOLUTION.py
+++ Python for Data Structures Algorithms/exercise/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/SOLUTIONS/Implement a Deque - SOLUTION.py
@@ -1,28 +1,28 @@
-# # %%
-# '''
-# # Implement a Deque - SOLUTION
-# **Please refer to the Implementation of Deque lecture for a full explanation. The code from that lecture is below:**
-# '''
-#
-# # %%
-# c_ Deque:
-#     ___ -
-#         items _    # list
-#
-#     ___ isEmpty
-#         r_ ? __    # list
-#
-#     ___ addFront item
-#         ?.a.. ?
-#
-#     ___ addRear item
-#         ?.i.. 0 ?
-#
-#     ___ removeFront
-#         r_ ?.p..
-#
-#     ___ removeRear
-#         r_ ?.p.. 0
-#
-#     ___ size
-#         r_ l.. ?
+# %%
+'''
+# Implement a Deque - SOLUTION
+**Please refer to the Implementation of Deque lecture for a full explanation. The code from that lecture is below:**
+'''
+
+# %%
+class Deque:
+    def __init__(self):
+        self.items = []   # list
+
+    def is_empty(self):
+        return self.items == []   # list
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
