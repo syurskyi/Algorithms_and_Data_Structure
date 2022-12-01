6a4b314c6a8d6170349ee50c8e18bee999c6449a
serge <sergejyurskyj@yahoo.com>
2022-11-30 21:20:12 -0800

    Committed stack exercises

--- The Complete Data Structures and Algorithms Course in Python/template/Section 19 Stack/stack.py
+++ The Complete Data Structures and Algorithms Course in Python/template/Section 19 Stack/stack.py
@@ -1,44 +1,44 @@
-# #   Created by Elshad Karimov on 19/05/2020.
-# #   Copyright © 2020 AppMillers. All rights reserved.
-#
-# c_ MultiStack
-#
-#     ___ -  stacksize
-#         numstacks _ 3
-#         array _ 0 * (? * ?
-#         sizes _ 0 * ?
-#         ? _ ?
-#         # print(self.array)
-#         # print(self.sizes)
-#
-#     ___ Push item, stacknum
-#         __ I.. ?
-#             r... E..('Stack is full')
-#         ? ? +_ 1
-#         a.. I.. ? _ ?
-#
-#     ___ Pop stacknum
-#         __ I.. ?
-#             r... E..('Stack is empty')
-#         value _ a.. I.. ?
-#         a.. I.. ? _ 0
-#         ? ? -_ 1
-#         r_ ?
-#
-#     ___ Peek stacknum
-#         __ I.. ?
-#             r... E..('Stack is empty')
-#         r_ a.. I.. ?
-#
-#     ___ IsEmpty stacknum
-#         r_ ? ? __ 0
-#
-#     ___ IsFull stacknum
-#         r_ ? ? __ ?
-#
-#     ___ IndexOfTop stacknum
-#         offset _ ? * ?
-#         r_ ? + ? ? - 1
-#
-# stack _ MultiStack(1)
-#
+#   Created by Elshad Karimov on 19/05/2020.
+#   Copyright © 2020 AppMillers. All rights reserved.
+
+class MultiStack:
+
+    def __init__(self, stacksize):
+        self.numstacks = 3
+        self.array = [0] * (stacksize * self.numstacks)
+        self.sizes = [0] * self.numstacks
+        self.stacksize = stacksize
+        # print(self.array)
+        # print(self.sizes)
+
+    def Push(self, item, stacknum):
+        if self.isFull(stacknum):
+            raise Exception('Stack is full')
+        self.sizes[stacknum] += 1
+        self.array[self.IndexOfTop(stacknum)] = item
+
+    def Pop(self, stacknum):
+        if self.IsEmpty(stacknum):
+            raise Exception('Stack is empty')
+        value = self.array[self.IndexOfTop(stacknum)]
+        self.array[self.IndexOfTop(stacknum)] = 0
+        self.sizes[stacknum] -= 1
+        return value
+
+    def Peek(self, stacknum):
+        if self.IsEmpty(stacknum):
+            raise Exception('Stack is empty')
+        return self.array[self.IndexOfTop(stacknum)]
+
+    def IsEmpty(self, stacknum):
+        return self.sizes[stacknum] == 0
+
+    def IsFull(self, stacknum):
+        return self.sizes[stacknum] == self.stacksize
+
+    def IndexOfTop(self, stacknum):
+        offset = stacknum * self.stacksize
+        return offset + self.sizes[stacknum] - 1
+
+stack = MultiStack(1)
+
