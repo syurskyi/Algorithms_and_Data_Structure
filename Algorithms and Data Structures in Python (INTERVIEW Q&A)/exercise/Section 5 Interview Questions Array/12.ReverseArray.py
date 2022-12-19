d1af4e4e5f37a3a06b1021e3e51cf057a6e8994e
serge <sergejyurskyj@yahoo.com>
2022-12-19 11:22:33 -0800

    Committed stack exercises

--- Algorithms and Data Structures in Python (INTERVIEW Q&A)/exercise/Section 5 Interview Questions Array/12.ReverseArray.py
+++ Algorithms and Data Structures in Python (INTERVIEW Q&A)/exercise/Section 5 Interview Questions Array/12.ReverseArray.py
@@ -1,20 +1,19 @@
-#
-# ? ? reverse nums
-#
-#     # pointing to the first item
-#     start_index _ 0
-#     # index pointing to the last item
-#     end_index = l.. ? -1
-#
-#     _____ ? > ?
-#         # keep swapping the items
-#         ? ? ? ? _ ? ?, ? ?
-#         s.. _ ? + 1
-#         e.. _ ? - 1
-#
-#
-# __ _____ __ ____
-#
-#     n = [-3, 0, 3]
-#     reverse(n)
-#     print(n)
+
+def reverse(nums):
+
+    # pointing to the first item
+    start_index = 0
+    # index pointing to the last item
+    end_index = len(nums) - 1
+
+    while end_index > start_index:
+        # keep swapping the items
+        nums[start_index], nums[end_index] = nums[end_index], nums[start_index]
+        start_index = start_index + 1
+        end_index = end_index - 1
+
+if __name__ == '__main__':
+
+    n = [-3, 0, 3]
+    reverse(n)
+    print(n)
