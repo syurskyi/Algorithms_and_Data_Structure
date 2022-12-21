bec1f499821159478f5a2bcb5c9030c18bd9cd82
serge <sergejyurskyj@yahoo.com>
2022-12-21 13:24:31 -0800

    Committed array exercises

--- LeetCode In Python/src/Section 5 INTERVIEW QUESTIONS Arrays and Strings/boats to save people/index.py
+++ LeetCode In Python/src/Section 5 INTERVIEW QUESTIONS Arrays and Strings/boats to save people/index.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
@@ -17,3 +20,14 @@ class Solution:
             right-=1
             boats_number+=1
         return boats_number
+
+
+s = Solution()
+# [2, 1, 3, 4], limit = 4
+# [1, 2, 3, 4]
+
+# [4]
+# [1, 3]
+# [2]
+answer = s.numRescueBoats([2, 1, 3, 4], 4)
+print(answer)
