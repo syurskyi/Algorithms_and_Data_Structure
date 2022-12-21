a67827908d87ed97bb56b4216dd5ab45942fe916
serge <sergejyurskyj@yahoo.com>
2022-12-20 17:19:25 -0800

    Committed array exercises

--- Cracking Coding Interviews - Mastering Algorithms/exercise/Section 2 Arrays and Strings/unique-characters.py
+++ Cracking Coding Interviews - Mastering Algorithms/exercise/Section 2 Arrays and Strings/unique-characters.py
@@ -1,26 +1,26 @@
-# # Implement an algorithm to determine if a string has all unique characters.
-#
-# # "abc" -> True
-# # "" -> True
-# # "aabc" -> False
-#
-# # Brute Force - O(n^2)
-# # Sorting - O(nlogn + n) -> O(nlogn)
-# # Hashset - O(n)
-#
-# ___ unique_characters word
-# 	visited_chars _ s..
-#
-# 	___ i __ r.. l.. ?
-# 		letter _ ? ?
-#
-# 		__ ? __ ?
-# 			r_ F..
-#
-# 		?.a.. ?
-#
-# 	r_ T..
-#
-# print(unique_characters("abc"))
-# print(unique_characters(""))
-# print(unique_characters("aabc"))
+# Implement an algorithm to determine if a string has all unique characters.
+
+# "abc" -> True
+# "" -> True
+# "aabc" -> False
+
+# Brute Force - O(n^2)
+# Sorting - O(nlogn + n) -> O(nlogn)
+# Hashset - O(n)
+
+def unique_characters(word):
+	visited_chars = set()
+
+	for i in range(len(word)):
+		letter = word[i]
+
+		if letter in visited_chars:
+			return False
+
+		visited_chars.add(letter)
+
+	return True
+
+print(unique_characters("abc"))
+print(unique_characters(""))
+print(unique_characters("aabc"))
